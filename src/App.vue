9e1b9d94cd13df4291db75b3a222e840d2d0ef14
Tommaso Bailetti <bailetti.tommaso@gmail.com>
2022-06-29 15:12:47 +0200

    Added text with computed component

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,24 @@
-<script setup lang="ts">
-import {ref} from 'vue'
+<script lang="ts" setup>
+import {computed, ref} from 'vue'
 
 const count = ref(0)
 
+const text = computed(() => {
+  if (count.value > 25) {
+    return "Fine, I'll keep counting. Clicks: " + count.value
+  } else if (count.value > 20) {
+    return "Gosh, still here?"
+  } else if (count.value > 10) {
+    return "What are you doing with your life?"
+  } else if (count.value > 0) {
+    return "Clicks: " + count.value
+  } else {
+    return "Click me!"
+  }
+})
+
 function increment() {
-  count.value++
+  count.value++;
 }
 </script>
 
@@ -12,7 +26,7 @@ function increment() {
   <div class="wrapper">
     <div class="box">
       <p class="pb-2">This is a Work in Progress site. <br> Here, a button to spend some time. <br></p>
-      <button class="button" @click="increment">{{ count !== 0 ? "Clicks: " + count : "Click me!" }}</button>
+      <button class="button" @click="increment">{{ text }}</button>
     </div>
   </div>
 </template>
